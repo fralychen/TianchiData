7ec9d3364c8de3105a57d32dc538d3aa772e63eb
fralychen <fralychen@gmail.com>
2020-07-22 22:09:12 +0800

    change some wrong

--- Car.py
+++ Car.py
@@ -18,19 +18,24 @@ from IPython.display import Image
 
 
 # %%
-Train_data = pd.read_csv('./datalab/car_info_test.csv')
+Train_data = pd.read_csv('./datalab/car_info_train.csv')
 Train_data.info()
 
 
 # %%
-Train_data['CUST_AGE'] = Train_data.CUST_AGE.fillna(Train_data.CUST_AGE.mean())
+Train_data['CUST_AGE'] = Train_data.CUST_AGE.fillna(Train_data.CUST_AGE.mean()) #均值填充
+
+# 分类
 Train_data['CUST_AGE'] = Train_data.CUST_AGE.apply(lambda x: 1 if (x<=35 and x >=16) else 2 if (x >= 36 and x <= 60) else 3)
 Train_data['CAR_AGE'] = Train_data.CAR_AGE.apply(lambda x: 1 if (x <=730) else 2 if (x >=731 and x <= 1460) else 3 if (x >= 1461 and x <= 2190) else 4 if (x >= 2191 and x <= 3650) else 5)
 Train_data['CAR_PRICE'] = Train_data.CAR_PRICE.apply(lambda x: 1 if (x >=50000 and x <= 90000) else 2 if (x >= 90001 and x<= 150000) else 3 if (x >= 150001 and x <= 300000) else 4)
 Train_data['LOAN_AMOUNT'] = Train_data.LOAN_AMOUNT.apply(lambda x: 1 if (x <= 50000) else 2 if(x >=50001 and x <= 200000) else 3 if(x >= 200001 and x <= 500000) else 4)
 
+Train_data['CAR_MODEL'] = Train_data.CAR_MODEL.apply(lambda x: re.findall('\d', x)[0]).astype(int) #转换 int
+
 
 # %%
+# 获取数字特征列
 numerical_cols = Train_data.select_dtypes(exclude = 'object').columns
 print(numerical_cols)
 
@@ -40,10 +45,12 @@ Train_data.head()
 
 
 # %%
+# 空值填充（0）
 Train_data.fillna(value=0,inplace=True)
 
 
 # %%
+# 判断空值
 Train_data.isnull().any()
 
 
@@ -51,15 +58,6 @@ Train_data.isnull().any()
 feature_cols = [col for col in numerical_cols if col != 'IS_LOST']
 data = Train_data[feature_cols].fillna(value=0)
 target = Train_data['IS_LOST']
-data
-
-
-# %%
-target
-
-
-# %%
-data.info()
 
 
 # %%
